bdfe4ab95db51a9b7d1d0722dcf131b11c9d282d
Zoli Kahan <zolikahan@gmail.com>
2013-07-20 01:00:09 -0500

    made browser compatible

--- promiz.js
+++ promiz.js
@@ -1,214 +1,229 @@
-var promiz = (function(){
-  function defer(){
+(function(){
 
-    this.state = 'pending'
-    this.value
-    this.stack = []
-    this.throwing = false
-
-    this.resolve = function(val){
-      if (this.state === 'pending'){
-        this.state = 'resolved'
-        this.fire(val)
-      }
-      return this
+  function nextTick(fn){
+    if(typeof process !== 'undefined' && process.nextTick) {
+      process.nextTick(fn)
+    } else {
+      setTimeout(fn, 0)
     }
+  }
 
-    this.reject = function(val){
-      if (this.state === 'pending'){
-        this.state = 'rejected'
-        this.fire(val)
-      }
-      return this
-    }
+  var promiz = (function(){
+    function defer(){
 
-    this.then = function(fn, er){
-      this.stack.push([fn || function(){}, er])
-      if (this.state !== 'pending') {
-          this.fire()
-      }
-      return this
-    }
+      this.state = 'pending'
+      this.value
+      this.stack = []
+      this.throwing = false
 
-    this.done = function(){
-      this.throwing = true
-      if (this.state !== 'pending') {
-          this.fire()
-      }
-      return void 0
-    }
-
-    this.throws = function(){
-      this.throwing = true
-      return this
-    }
-
-    this.catch = function (fn) {
-      this.stack.push([null, fn || function(){}])
-      if (this.state !== 'pending') {
-          this.fire()
-      }
-      return this
-    }
-
-    this.fail = function (fn) {
-      return this.catch(fn)
-    }
-
-    this.nodeify = function (cb) {
-      if(cb) {
-        this.then(function(val){
-          process.nextTick(function(){
-            cb(null, val)
-          })
-          return val
-        }, function(err){
-          process.nextTick(function(){
-            cb(err)
-          })
-        })
-      }
-      return this
-    }
-
-    this.spread = function (fn, er) {
-      return this.all().then(function(list){
-           return fn ? fn.apply(void 0, list) :  null
-      }, er)
-    }
-
-    this.all = function () {
-      var self = this
-
-      this.stack.push([function(list){
-
-        list = list instanceof Array ? list : []
-
-        if (list.length === 0){
-          return list
+      this.resolve = function(val){
+        if (this.state === 'pending'){
+          this.state = 'resolved'
+          this.fire(val)
         }
+        return this
+      }
 
-        self.state = 'pending'
-        var cnt = 0
-        var errored = false
+      this.reject = function(val){
+        if (this.state === 'pending'){
+          this.state = 'rejected'
+          this.fire(val)
+        }
+        return this
+      }
 
-        function checkDone(){
-          if(cnt !== list.length) {
-            return
+      this.then = function(fn, er){
+        this.stack.push([fn || function(){}, er])
+        if (this.state !== 'pending') {
+          this.fire()
+        }
+        return this
+      }
+
+      this.done = function(){
+        this.throwing = true
+        if (this.state !== 'pending') {
+          this.fire()
+        }
+        return void 0
+      }
+
+      this.throws = function(){
+        this.throwing = true
+        return this
+      }
+
+      this.catch = function (fn) {
+        this.stack.push([null, fn || function(){}])
+        if (this.state !== 'pending') {
+          this.fire()
+        }
+        return this
+      }
+
+      this.fail = function (fn) {
+        return this.catch(fn)
+      }
+
+      this.nodeify = function (cb) {
+        if(cb) {
+          this.then(function(val){
+            nextTick(function(){
+              cb(null, val)
+            })
+            return val
+          }, function(err){
+            nextTick(function(){
+              cb(err)
+            })
+          })
+        }
+        return this
+      }
+
+      this.spread = function (fn, er) {
+        return this.all().then(function(list){
+          return fn ? fn.apply(void 0, list) :  null
+        }, er)
+      }
+
+      this.all = function () {
+        var self = this
+
+        this.stack.push([function(list){
+
+          list = list instanceof Array ? list : []
+
+          if (list.length === 0){
+            return list
           }
-          self.resolve(list)
-        }
 
-        list.forEach(function(val, i){
-          if(val && val.then){
+          self.state = 'pending'
+          var cnt = 0
+          var errored = false
 
-            val.then(function(res){
-              list[i] = res
+          function checkDone(){
+            if(cnt !== list.length) {
+              return
+            }
+            self.resolve(list)
+          }
+
+          list.forEach(function(val, i){
+            if(val && val.then){
+
+              val.then(function(res){
+                list[i] = res
+                cnt++
+                checkDone()
+              }, function(err){
+                self.reject(err)
+              })
+            } else {
+              list[i] = val
               cnt++
               checkDone()
-            }, function(err){
-              self.reject(err)
-            })
-          } else {
-            list[i] = val
-            cnt++
-            checkDone()
-          }
+            }
 
-        })
+          })
 
-        return list
-      }, null])
+          return list
+        }, null])
 
-      if (this.state !== 'pending') {
+        if (this.state !== 'pending') {
           this.fire()
+        }
+
+        return this
       }
 
-      return this
-    }
+      this.fire = function(val){
+        val = this.value = typeof val !== 'undefined' ? val : this.value
 
-    this.fire = function(val){
-      val = this.value = typeof val !== 'undefined' ? val : this.value
+        while(this.stack.length && this.state !== 'pending') {
+          var entry = this.stack.shift()
+          var fn = this.state === 'rejected' ? entry[1] : entry[0]
 
-      while(this.stack.length && this.state !== 'pending') {
-        var entry = this.stack.shift()
-        var fn = this.state === 'rejected' ? entry[1] : entry[0]
+          if(fn) {
+            try {
 
-        if(fn) {
-          try {
+              val = this.value = fn.call(void 0, val)
+              if(val && val.then) {
+                var previousState = this.state
+                this.state = 'pending'
+                var promise = val.then(function(v){
 
-            val = this.value = fn.call(void 0, val)
-            if(val && val.then) {
-              var previousState = this.state
-              this.state = 'pending'
-              var promise = val.then(function(v){
+                  val = this.value = v
 
-                val = this.value = v
+                  this.resolve(v)
+                }.bind(this), function(err){
+                  val = this.value = err
+                  if(previousState !== 'rejected') this.stack.unshift(entry)
+                  this.reject(err)
+                }.bind(this))
 
-                this.resolve(v)
-              }.bind(this), function(err){
-                val = this.value = err
-                if(previousState !== 'rejected') this.stack.unshift(entry)
-                this.reject(err)
-              }.bind(this))
-
-            } else if (this.state === 'rejected') {
-              this.state = 'resolved'
-            }
-          } catch (e) {
-            val = this.value = e
-            if(this.state !== 'rejected') this.stack.unshift(entry)
-            this.state = 'rejected'
+              } else if (this.state === 'rejected') {
+                this.state = 'resolved'
+              }
+                } catch (e) {
+                  val = this.value = e
+                  if(this.state !== 'rejected') this.stack.unshift(entry)
+                  this.state = 'rejected'
+                }
           }
+        }
+
+        if(this.throwing && this.stack.length === 0 && this.state === 'rejected') {
+          throw this.value
         }
       }
 
-      if(this.throwing && this.stack.length === 0 && this.state === 'rejected') {
-        throw this.value
-      }
     }
 
+    return {
+      defer: function(){
+        return new defer()
+      },
+
+      fcall: function() {
+        var deferred = new defer()
+        var args = Array.apply([], arguments)
+        var fn = args.shift()
+        try {
+          var val = fn.apply(void 0, args)
+          deferred.resolve(val)
+        } catch(e) {
+          deferred.reject(e)
+        }
+
+        return deferred
+      },
+
+      nfcall: function() {
+        var deferred = new defer()
+        var args = Array.apply([], arguments)
+        var fn = args.shift()
+        try {
+          args.push(function(err, val){
+            if(err) {
+              return deferred.reject(err)
+            }
+            return deferred.resolve(val)
+          })
+          fn.apply(void 0, args)
+        } catch(e){
+          deferred.reject(e)
+        }
+
+        return deferred
+      },
+    }
+
+  })()
+
+  if(typeof module !== 'undefined' && module.exports) {
+    module.exports = promiz
+  } else {
+    this.promiz = promiz
   }
-
-  return {
-    defer: function(){
-      return new defer()
-    },
-
-    fcall: function() {
-      var deferred = new defer()
-      var args = Array.apply([], arguments)
-      var fn = args.shift()
-      try {
-        var val = fn.apply(void 0, args)
-        deferred.resolve(val)
-      } catch(e) {
-        deferred.reject(e)
-      }
-
-      return deferred
-    },
-
-    nfcall: function() {
-      var deferred = new defer()
-      var args = Array.apply([], arguments)
-      var fn = args.shift()
-      try {
-        args.push(function(err, val){
-          if(err) {
-            return deferred.reject(err)
-          }
-          return deferred.resolve(val)
-        })
-        fn.apply(void 0, args)
-      } catch(e){
-        deferred.reject(e)
-      }
-
-      return deferred
-    },
-  }
-
 })()
-
-module.exports = promiz
