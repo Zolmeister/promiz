fba5634fd4a97ce560a6167008cdb910493bb15c
Zoli Kahan <zolikahan@gmail.com>
2014-01-06 00:21:14 -0600

    added command to test micro version

---
+++ spec-adapter-micro.js
@@ -0,0 +1,14 @@
+var Promiz = require('./promiz.micro');
+module.exports = {
+  resolved: function (value) {
+    var d = Promiz.defer();
+    d.resolve(value);
+    return d;
+  },
+  rejected: function (error) {
+    var d = Promiz.defer();
+    d.reject(error);
+    return d;
+  },
+  deferred: Promiz.defer
+};
